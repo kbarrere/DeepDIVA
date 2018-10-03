edb7952c9c3b44a68826f0a0a2554292a93b2538
kbarrere <killian.barrere@free.fr>
2018-10-03 17:22:35 +0200

    dataloader set to use custom one from dataset folder

--- template/runner/handwritten_text_recognition/setup.py
+++ template/runner/handwritten_text_recognition/setup.py
@@ -8,11 +8,11 @@ import os
 import torchvision.transforms as transforms
 
 # DeepDIVA
-from datasets.image_folder_triplet import load_dataset
+from datasets.piff_line_dataset import load_dataset
 from template.setup import _dataloaders_from_datasets, _load_mean_std_from_file
 
 
-def setup_dataloaders(model_expected_input_size, dataset_folder, n_triplets, batch_size, workers, inmem, **kwargs):
+def setup_dataloaders(model_expected_input_size, piff_json_file, batch_size, workers, inmem, **kwargs):
     """
     Set up the dataloaders for the specified datasets.
 
@@ -20,10 +20,8 @@ def setup_dataloaders(model_expected_input_size, dataset_folder, n_triplets, bat
     ----------
     model_expected_input_size : tuple
         Specify the height and width that the model expects.
-    dataset_folder : string
-        Path string that points to the three folder train/val/test. Example: ~/../../data/svhn
-    n_triplets : int
-        Number of triplets to generate for train/val/tes
+    piff_json_file : string
+        File describing the dataset following the PiFF format
     batch_size : int
         Number of datapoints to process at once
     workers : int
@@ -42,33 +40,20 @@ def setup_dataloaders(model_expected_input_size, dataset_folder, n_triplets, bat
     """
 
     # Recover dataset name
-    dataset = os.path.basename(os.path.normpath(dataset_folder))
-    logging.info('Loading {} from:{}'.format(dataset, dataset_folder))
+    file_name = os.path.basename(os.path.normpath(piff_json_file))
+    logging.info('Loading {} from:{}'.format(file_name, piff_json_file))
 
     ###############################################################################################
     # Load the dataset splits as images
-    train_ds, val_ds, test_ds = load_dataset(dataset_folder=dataset_folder,
+    train_ds, val_ds, test_ds = load_dataset(piff_json_file=piff_json_file,
                                              in_memory=inmem,
-                                             workers=workers,
-                                             num_triplets=n_triplets)
+                                             workers=workers)
 
-    # Loads the analytics csv and extract mean and std
-    mean, std = _load_mean_std_from_file(dataset_folder=dataset_folder,
-                                         inmem=inmem,
-                                         workers=workers)
 
     # Set up dataset transforms
     logging.debug('Setting up dataset transforms')
 
-    standard_transform = transforms.Compose([
-        transforms.Resize(size=model_expected_input_size),
-        transforms.ToTensor(),
-        transforms.Normalize(mean=mean, std=std)
-    ])
-
-    train_ds.transform = standard_transform
-    val_ds.transform = standard_transform
-    test_ds.transform = standard_transform
+    # TODO : transforms
 
     train_loader, val_loader, test_loader = _dataloaders_from_datasets(batch_size=batch_size,
                                                                        train_ds=train_ds,
