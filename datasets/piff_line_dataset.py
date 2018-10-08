69e7801a4648dd28e264f80673492dfeb71434d4
kbarrere <killian.barrere@free.fr>
2018-10-08 14:28:44 +0200

    created CRNN.py from AlexNet

--- datasets/piff_line_dataset.py
+++ datasets/piff_line_dataset.py
@@ -176,17 +176,149 @@ class LineImageNotInMemory(data.Dataset):
             img = Image.open(f)
             # Copy the image to avoid bug when the file is closed later
             img = img.copy()
-            img = img.resize([227, 227], Image.ANTIALIAS) # TODO remove
+            img = img.resize([227, 227], Image.ANTIALIAS)  # TODO remove
 
-        #target = self.line_values[index]
+        # target = self.line_values[index]
 
-        #target = torch.LongTensor([0])
+        # target = torch.LongTensor([0])
         target = 0
         # TODO Remove
         if 'P' in self.line_values[index]:
-            #target = torch.LongTensor([1])
+            # target = torch.LongTensor([1])
             target = 1
 
+        if self.transform is not None:
+            img = self.transform(img)
+        if self.target_transform is not None:
+            target = self.target_transform(target)
+
+        return img, target
+
+    def __len__(self):
+        return len(self.image_paths)
+
+
+class LineImageInMemory(data.Dataset):
+    """
+    TODO
+    """
+
+    def __init__(self, piff_json_file, split, transform=None, target_transform=None, workers=1):
+        """
+        Load the data in memory and prepares it as a dataset.
+
+        Parameters
+        ----------
+        piff_json_file : string
+            Path to the json file following the PiFF format
+            This file contains the line value and the path to the image line
+        split: string
+            Indicates which split it should load
+            Should be either train, val or test
+        transform : torchvision.transforms
+            Transformation to apply on the data
+        target_transform : torchvision.transforms
+            Transformation to apply on the labels
+        workers: int
+            Number of workers to use for the dataloaders
+        """
+
+        if split not in ["training", "validation", "test"]:
+            logging.error("provided split (" + split + ") is not train, valid or test")
+            sys.exit(-1)
+
+        self.piff_json_file = piff_json_file
+        self.split = split
+        self.transform = transform
+        self.target_transform = target_transform
+
+        self.piff_json_folder = self.piff_json_file[:-len(self.piff_json_file.split('/')[-1])]
+        self.image_paths = []
+        self.line_values = []
+
+        f = open(piff_json_file, 'r')
+        piff_dict = json.load(f)
+
+        self.line_split_search(piff_dict, False)
+
+        len_images_path = len(self.image_paths)
+        len_line_values = len(self.line_values)
+
+        if len_images_path != len_line_values:
+            logging.error("Error while loading PiFF Json file !")
+            logging.error("Found " + str(len_images_path) + "image path but " + str(len_line_values) +
+                          "line transcriptions.")
+            sys.exit(-1)
+
+        # TODO remove, temp classification task to test
+        self.labels = ['P' in item for item in self.line_values]
+        self.classes = np.unique(self.labels)
+
+        logging.info("Loaded " + str(self.__len__()) + " lines for the " + self.split + " split.")
+
+        f.close()
+
+    def line_split_search(self, dict, is_in_split=False):
+        if is_in_split:
+            if 'type' in dict and dict['type'] == 'line':
+                path = ""
+                if 'path' in dict:
+                    path = dict['path']
+
+                self.image_paths.append(self.piff_json_folder + path)
+
+                value = ""
+                if 'value' in dict:
+                    value = dict['value']
+
+                self.line_values.append(value)
+
+            else:
+                if 'children' in dict:
+                    for child in dict['children']:
+                        self.line_split_search(child, is_in_split=True)
+
+        else:
+            if 'split' in dict:
+                if dict['split'] == self.split:
+                    self.line_split_search(dict, is_in_split=True)
+                else:
+                    return
+            else:
+                if 'children' in dict:
+                    for child in dict['children']:
+                        self.line_split_search(child, is_in_split=False)
+
+    def __getitem__(self, index):
+        """
+        Retrieve a sample by index
+
+        Parameters
+        ----------
+        index : int
+
+        Returns
+        -------
+        img : FloatTensor
+        target : string
+            transciption of the text inside the image
+        """
+
+        # Weird way to open things due to issue https://github.com/python-pillow/Pillow/issues/835
+        with open(self.image_paths[index], 'rb') as f:
+            img = Image.open(f)
+            # Copy the image to avoid bug when the file is closed later
+            img = img.copy()
+            img = img.resize([227, 227], Image.ANTIALIAS)  # TODO remove
+
+        # target = self.line_values[index]
+
+        # target = torch.LongTensor([0])
+        target = 0
+        # TODO Remove
+        if 'P' in self.line_values[index]:
+            # target = torch.LongTensor([1])
+            target = 1
 
         if self.transform is not None:
             img = self.transform(img)
