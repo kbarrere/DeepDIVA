3c4d226afe049fa14d92334eb942a7763765a4d2
kbarrere <killian.barrere@free.fr>
2018-10-11 10:02:11 +0200

    convs and collapse, but still with fc and classif task

--- models/CRNN.py
+++ models/CRNN.py
@@ -17,6 +17,20 @@ class Flatten(nn.Module):
         return x
 
 
+class Collapse(nn.Module):
+    """
+    TODO
+    """
+    def __init__(self, height=23, width=1):
+        super(Collapse, self).__init__()
+        self.mp = nn.MaxPool2d(kernel_size=(height, width))
+
+    def forward(self, x):
+        x = self.mp(x)
+        x = x.view(x.size()[0], x.size()[1], -1)
+        return x
+
+
 class _CRNN(nn.Module):
     r"""
 
@@ -52,29 +66,36 @@ class _CRNN(nn.Module):
         self.conv1 = nn.Sequential(
             nn.Conv2d(3, 8, kernel_size=3, stride=1, padding=0),
             nn.LeakyReLU(),
-            nn.MaxPool2d(kernel_size=3, stride=2),
+            nn.MaxPool2d(kernel_size=2, stride=2),
+            nn.BatchNorm2d(8)
         )
         self.conv2 = nn.Sequential(
             nn.Conv2d(8, 16, kernel_size=3, stride=1, padding=0),
             nn.LeakyReLU(),
-            nn.MaxPool2d(kernel_size=3, stride=2),
+            nn.MaxPool2d(kernel_size=2, stride=2),
+            nn.BatchNorm2d(16)
         )
         self.conv3 = nn.Sequential(
             nn.Conv2d(16, 32, kernel_size=3, stride=1, padding=0),
             nn.LeakyReLU(),
+            nn.Dropout2d(p=0.5),
+            nn.BatchNorm2d(32)
         )
         self.conv4 = nn.Sequential(
-            nn.Conv2d(32, 64, kernel_size=(2, 4), stride=1, padding=0),
+            nn.Conv2d(32, 64, kernel_size=3, stride=1, padding=0),
             nn.LeakyReLU(),
+            nn.Dropout2d(p=0.5),
+            nn.BatchNorm2d(64)
         )
 
         self.conv5 = nn.Sequential(
-            nn.Conv2d(64, 128, kernel_size=3, stride=1, padding=0),
+            nn.Conv2d(64, 128, kernel_size=(4, 2), stride=1, padding=0),
             nn.LeakyReLU(),
-            nn.MaxPool2d(kernel_size=3, stride=2),
-            nn.Dropout2d()
+            nn.Dropout2d(p=0.5),
+            nn.BatchNorm2d(128)
         )
 
+        """
         # Fully connected layers
         self.fc1 = nn.Sequential(
             Flatten(),
@@ -90,6 +111,14 @@ class _CRNN(nn.Module):
         # Classification layer
         self.cl = nn.Sequential(
             nn.Linear(4096, output_channels),
+        )
+        """
+
+        # Classification layer
+        self.cl = nn.Sequential(
+            Collapse(height=23, width=505),
+            Flatten(),
+            nn.Linear(128, output_channels),
         )
 
     def forward(self, x):
@@ -111,8 +140,10 @@ class _CRNN(nn.Module):
         x = self.conv3(x)
         x = self.conv4(x)
         x = self.conv5(x)
+        """
         x = self.fc1(x)
         x = self.fc2(x)
+        """
         x = self.cl(x)
         return x
 
